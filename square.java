10ecade75bf30d37328d37c9b3b811274cad8cc2
chidanandmali8484 <106953817+chidanandmali8484@users.noreply.github.com>
2022-06-24 13:05:10 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,22 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		
+       
+     
+       int i = 1;
+       int sum = 0;
+       System.out.println("\t\t\tnumber  square");
+       
+       while(i<=10)
+       {
+           int sq = i*i;
+           System.out.println("\t\t\t"+i+"  "+sq);
+           sum=sum+sq;
+           
+          i++;
+	}
+	    System.out.println("\t\t\tTotal\t"+sum);
+  }
+ }
